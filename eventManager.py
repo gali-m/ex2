26b1fff1b076df623ab8cbab8e7a2d9c93a5e2ce
Gali Michlevich <gali@campus.technion.ac.il>
2020-12-23 23:18:17 +0200

    SWIG

--- eventManager.py
+++ eventManager.py
@@ -200,15 +200,15 @@ def printEventsList(events :list,file_path :str): #em, event_names: list, event_
     #TODO 3.2.2
     
     
-# def testPrintEventsList(file_path :str):
-#     events_lists=[{"name":"New Year's Eve","id":1,"date": EM.dateCreate(30, 12, 2020)},\
-#                     {"name" : "annual Rock & Metal party","id":2,"date":  EM.dateCreate(21, 4, 2021)}, \
-#                                  {"name" : "Improv","id":3,"date": EM.dateCreate(13, 3, 2021)}, \
-#                                      {"name" : "Student Festival","id":4,"date": EM.dateCreate(13, 5, 2021)},    ]
-#     em = printEventsList(events_lists,file_path)
-#     for event in events_lists:
-#         EM.dateDestroy(event["date"])
-#     EM.destroyEventManager(em)
+def testPrintEventsList(file_path :str):
+    events_lists=[{"name":"New Year's Eve","id":1,"date": EM.dateCreate(30, 12, 2020)},\
+                    {"name" : "annual Rock & Metal party","id":2,"date":  EM.dateCreate(21, 4, 2021)}, \
+                                 {"name" : "Improv","id":3,"date": EM.dateCreate(13, 3, 2021)}, \
+                                     {"name" : "Student Festival","id":4,"date": EM.dateCreate(13, 5, 2021)},    ]
+    em = printEventsList(events_lists,file_path)
+    for event in events_lists:
+        EM.dateDestroy(event["date"])
+    EM.destroyEventManager(em)
 
 #### Main #### 
 # feel free to add more tests and change that section. 
@@ -216,5 +216,5 @@ def printEventsList(events :list,file_path :str): #em, event_names: list, event_
 if __name__ == "__main__":
     import sys
     if len(sys.argv)>1:
-        pass
-        # testPrintEventsList(sys.argv[1])
+        testPrintEventsList(sys.argv[1])
+    print("pass")
