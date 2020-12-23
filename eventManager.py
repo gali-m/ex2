c453cec5e85fea9ae348a120e770245ad4e29e41
Gali Michlevich <gali@campus.technion.ac.il>
2020-12-23 18:50:03 +0200

    3.1.1

--- eventManager.py
+++ eventManager.py
@@ -1,14 +1,69 @@
 #### IMPORTS ####
 import event_manager as EM
 
-#### PART 1 ####
+def isValid(id, name, age, birth_year, semester):
+    if type(id) is not int or id < 10000000 or id > 99999999:
+        return False
+    
+    if type(name) is not str:
+        return False
+    words = name.split(" ")
+    for word in words:
+        if word.isalpha() == False:
+            return False
+    
+    if type(age) is not int or age < 16 or age > 120:
+        return False
+    
+    if type(birth_year) is not int or 2020 - age != birth_year:
+        return False
+    
+    if type(semester) is not int or semester < 1:
+        return False
+
+
+def cleanName(name):
+    clean_name = ''
+    words = name.split(" ")
+    for i,word in enumerate(words):
+        if i != 0:
+            clean_name += ' '
+        clean_name += word
+    return clean_name
+
+
 # Filters a file of students' subscription to specific event:
 #   orig_file_path: The path to the unfiltered subscription file
 #   filtered_file_path: The path to the new filtered file
 def fileCorrect(orig_file_path: str, filtered_file_path: str):
-    pass
-    #TODO 3.1.1
-    
+    list=[]
+
+    src_file = open(orig_file_path,'r')
+
+    for line in src_file:
+        id, name, age, birth_year, semester = str.split(",")
+        id = id.strip()
+        name = cleanName(name)
+        age = age.strip()
+        birth_year = birth_year.strip()
+        semester = semester.strip()
+
+        if isValid(id, name, age, birth_year, semester):
+            tmp_list = [id, name, age, birth_year, semester]
+            for student in list:
+                if student[0] == id:
+                    list.remove(student)
+            list.append(tmp_list)
+
+    src_file.close()
+    list.sort()
+
+    dest_file = open(orig_file_path,'w')
+
+    for student in list:
+        dest_file.write(student[0] + " , " +student[1] + " , " + student[2] + " , " + student[3] + " , " + student[4])
+        
+    dest_file.close()
 
 # Writes the names of the K youngest students which subscribed 
 # to the event correctly.
